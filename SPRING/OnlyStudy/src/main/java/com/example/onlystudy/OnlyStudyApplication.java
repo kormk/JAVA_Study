63b602c489edddf03d5cf1c35c7bf2addd40d5eb
kormk <63334787+kormk@users.noreply.github.com>
2023-09-07 16:30:18 +0900

    🎉 init

---
+++ SPRING/OnlyStudy/src/main/java/com/example/onlystudy/OnlyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlyStudyApplication.class, args);
+    }
+
+}
---
+++ SPRING/OnlyStudy/src/test/java/com/example/onlystudy/OnlyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
